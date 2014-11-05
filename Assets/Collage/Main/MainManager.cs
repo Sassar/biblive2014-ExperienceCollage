4f999acc4d0075f027f87582e414b56d964d4c79
Sassar <ta1.sakura96@gmail.com>
2014-11-05 16:11:47 +0900

    HMの棚オブジェクトのリネームとマテリアルをアタッチ
    ・HMの棚オブジェクトをリネームした。
    ・HMの棚オブジェクトに応じたマテリアルを作成＆アタッチをした
    ・MainManagerのShelf_Obj_Namesの中身(棚オブジェクト名)を追記した
    ・MainManagerで、映像の長さが最大or最小のものを見つけ出すためのソートquickを実装した

--- Assets/Collage/Main/MainManager.cs
+++ Assets/Collage/Main/MainManager.cs
@@ -6,33 +6,44 @@ public class MainManager : MonoBehaviour {
 	public float screenW;
 
 	private string One_Of_SN;	// SN:Shelf Name
-	private string[] Shelf_Obj_Names = {"B1_HM", "B2_HM"};	// need to fix!!!!!!!!!!!!!
+	private string[] Shelf_Obj_Names = {
+		"A3-01_sh", "A3-02_sh", "A4_sh", "A5_sh",
+				  "B01_sh", "B02_HM", "B03_sh", "B04_sh", "B05_sh", "B06_sh", "B07_sh", "B08_sh", "B09_sh",
+		"B10_sh", "B11_sh", "B12_sh", "B13_sh", "B14_sh", "B15_sh", "B16_sh", "B17_sh", "B18_sh", "B19_sh",
+		"B20_sh", "B21_sh", "B22_sh", "B23_sh", "B24_sh", "B25_sh", "B26_sh", "B27_sh", "B28_sh", "B29-01_sh", "B29-02_sh",
+		"B30_sh", "B31_sh", "B32_sh", "B33_sh", "B34_sh", "B35_sh", "B36_sh", "B37_sh", "B38_sh", "B39_sh",
+		"B40_sh", "B41_sh", "B42_sh", "B43_sh", 
+		"D01_sh", "D02_sh", "D03_sh", "D04_sh", "D05_sh", "D06_sh", "D07_sh",
+	};
+	private string[] Shelf_Name;
 	private int num_Of_SO;	// SO:Shelfs Object
 	private float[] Movie_lengths = new float[130];
 	public	float Length_Of_LM;	//LM:Longest movie
 	public	float Length_Of_SM;	//SM:Shotest movie
 	private MovieTexture mov_Texture;
+//	private AssetImporter mov;
 	private int i =0;
 
 	// Use this for initialization
 	void Start () {
 		screenW = Screen.width;
-
 		num_Of_SO = Shelf_Obj_Names.Length;
+
 		//get movies' length 
-//		for(i=0; i < num_Of_SO; i++){
+		for(i=0; i<num_Of_SO; i++){
+//			Shelf_Name[i] = Shelf_Obj_Names[i].Replace("_HM", "");
+//			mov = GameObject.Find (Shelf_Name);
+
 //			mov_Texture = GameObject.Find (Shelf_Obj_Names[i]).renderer.material.mainTexture as MovieTexture;
 //			Movie_lengths[i] = mov_Texture.duration;
-//		}
-//		
-//		//get most longest & shotest length in all movies
-//		for(i=0; i<num_Of_SO; i++){
-//			if(Length_Of_LM < Movie_lengths[i]){
-//				Length_Of_LM = Movie_lengths[i];
-//			} else if(Length_Of_SM > Movie_lengths[i]){
-//				Length_Of_SM = Movie_lengths[i];
-//			}
-//		}
+		}
+
+
+		//		//get most longest & shotest length in all movies
+		quick(Movie_lengths, 0, num_Of_SO-1);
+		Length_Of_LM = Movie_lengths[0];
+		Length_Of_SM = Movie_lengths[num_Of_SO-1];
+
 	}
 
 	// Update is called once per frame
@@ -47,5 +58,28 @@ public class MainManager : MonoBehaviour {
 		if (GUI.Button (new Rect (screenW-55, 10, 50, 25), "Title")) {
 			Application.LoadLevel(0);
 		}
+	}
+
+	//quick sort
+	void quick(float[] a, int left, int right){
+		int pl = left;	//left cursor
+		int pr = right;	//right cursor
+		float centerF = (pl + pr) / 2;
+		int centerI= (int) centerF;
+		float x = a[centerI];	//center
+		float temp;
+		do{
+			while(a[pl] < x)	pl++;
+			while(a[pr] > x)	pr--;
+			if(pl <= pr){
+				temp = a[pl];
+				a[pl] = a[pr];
+				a[pr] = temp;
+				pl++;
+				pl--;
+			}
+		} while(pl <= pr);
+		if(left < pr)	quick(a, left, pr);
+		if(pl < right)	quick(a, pl, right);
 	}
 }
